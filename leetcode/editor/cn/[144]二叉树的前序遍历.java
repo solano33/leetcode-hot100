e3a3343220ab52d56daab50c4e474ed48a2fbddc
solano-h <solano33@163.com>
2024-10-25 23:44:02 +0800

    feat: lc144 589 19

---
+++ leetcode/editor/cn/[144]二叉树的前序遍历.java
@@ -0,0 +1,112 @@
+//给你二叉树的根节点 root ，返回它节点值的 前序 遍历。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+// 输入：root = [1,null,2,3] 
+// 
+//
+// 输出：[1,2,3] 
+//
+// 解释： 
+//
+// 
+//
+// 示例 2： 
+//
+// 
+// 输入：root = [1,2,3,4,5,null,8,null,null,6,7,9] 
+// 
+//
+// 输出：[1,2,4,5,6,7,3,8,9] 
+//
+// 解释： 
+//
+// 
+//
+// 示例 3： 
+//
+// 
+// 输入：root = [] 
+// 
+//
+// 输出：[] 
+//
+// 示例 4： 
+//
+// 
+// 输入：root = [1] 
+// 
+//
+// 输出：[1] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：递归算法很简单，你可以通过迭代算法完成吗？ 
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1287 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        return fun2(root);
+    }
+
+    // 递归
+    public List<Integer> fun1(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        result.add(root.val);
+        result.addAll(fun1(root.left));
+        result.addAll(fun1(root.right));
+        return result;
+    }
+
+    // 迭代
+    public List<Integer> fun2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            // 注意先压右边的
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/main/java/com/solano/leetcode/Test_589.java
@@ -0,0 +1,19 @@
+package com.solano.leetcode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author github.com/solano33
+ * @date 2024/10/25 23:35
+ */
+public class Test_589 {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        for (Integer integer : list) {
+
+        }
+    }
+}
