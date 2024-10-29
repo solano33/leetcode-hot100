e313a39874cf47df867d8abf6b5f01bee237b99c
solano-h <solano33@163.com>
2024-10-30 01:48:41 +0800

    feat: lc 136 137 260 49

---
+++ leetcode/editor/cn/[49]字母异位词分组.java
@@ -0,0 +1,60 @@
+//给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。 
+//
+// 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出: [["bat"],["nat","tan"],["ate","eat","tea"]] 
+//
+// 示例 2: 
+//
+// 
+//输入: strs = [""]
+//输出: [[""]]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: strs = ["a"]
+//输出: [["a"]] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= strs.length <= 10⁴ 
+// 0 <= strs[i].length <= 100 
+// strs[i] 仅包含小写字母 
+// 
+//
+// Related Topics 数组 哈希表 字符串 排序 👍 2028 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return fun1(strs);
+    }
+
+    public List<List<String>> fun1(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        if (strs == null || strs.length == 0) return res;
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(strs[i]);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/main/java/com/solano/leetcode/Test_49.java
@@ -0,0 +1,13 @@
+package com.solano.leetcode;
+
+/**
+ * @author github.com/solano33
+ * @date 2024/10/29 23:20
+ */
+public class Test_49 {
+
+    public static void main(String[] args) {
+        String s = "adnjsknfdkjsd";
+
+    }
+}
