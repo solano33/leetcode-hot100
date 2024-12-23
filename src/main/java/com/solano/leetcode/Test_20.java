97ce7963ac267bf9c7ee861a3f55b83ead8e2495
solano-h <solano33@163.com>
2024-12-23 23:57:02 +0800

    feat: lc 20

---
+++ src/main/java/com/solano/leetcode/Test_20.java
@@ -0,0 +1,17 @@
+package com.solano.leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author github.com/solano33
+ * @date 2024/12/22 14:51
+ */
+public class Test_20 {
+
+    public static void main(String[] args) {
+        List<Character> list = Arrays.asList('(', '{', '[');
+        list.indexOf('(');
+        list.contains(1);
+    }
+}
